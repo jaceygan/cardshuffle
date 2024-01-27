301a73e75e4957f89fc9bd2048bba1fbc11b6c63
jaceygan <jaceygan@gmail.com>
2024-01-27 22:41:26 +0800

    more code cleanup

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 newDeckOrder = ["A♠️", "2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️", "8♠️", "9♠️", "10♠️", "J♠️", "Q♠️", "K♠️",
-        "A♦️", "2♦️", "3♦️", "4♦️", "5♦️", "6♦️", "7♦️", "8♦️", "9♦️", "10♦️", "J♦️", "Q♦️", "K♦️",
-        'K♣️', 'Q♣️', 'J♣️', '10♣️', '9♣️', '8♣️', '7♣️', '6♣️', '5♣️', '4♣️', '3♣️', '2♣️', 'A♣️',
-        'K♥️', 'Q♥️', 'J♥️', '10♥️', '9♥️', '8♥️', '7♥️', '6♥️', '5♥️', '4♥️', '3♥️', '2♥️', 'A♥️']
+                "A♦️", "2♦️", "3♦️", "4♦️", "5♦️", "6♦️", "7♦️", "8♦️", "9♦️", "10♦️", "J♦️", "Q♦️", "K♦️",
+                'K♣️', 'Q♣️', 'J♣️', '10♣️', '9♣️', '8♣️', '7♣️', '6♣️', '5♣️', '4♣️', '3♣️', '2♣️', 'A♣️',
+                'K♥️', 'Q♥️', 'J♥️', '10♥️', '9♥️', '8♥️', '7♥️', '6♥️', '5♥️', '4♥️', '3♥️', '2♥️', 'A♥️']
 
 def formatDeck(d):
     linesize = int((len(d))/4)
@@ -19,12 +19,10 @@ def formatDeck(d):
     output = output[:-5] # remove the last comma and line break
     return (output.replace("'",""))
 
-
 def isNDO(d):
     return d==newDeckOrder
 
 def faroShuffle(d, type="in"):
-
     half = int(len(d)/2) #TODO: need to handle odd numbers
     p1 = d[:half]
     p2 = d[half:]
@@ -38,22 +36,20 @@ def faroShuffle(d, type="in"):
         for i in range(half):
             newD.append(p2[i])
             newD.append(p1[i])
-    
     return newD
-
-@app.route("/cardshuffle")
-def cardshuffle():
-    deck = newDeckOrder
-    return render_template("index.html", isNDO = (deck==newDeckOrder),displayDeck=formatDeck(deck))
 
 @app.route("/")
 def index():
     return redirect(url_for("cardshuffle"))
 
+@app.route("/cardshuffle")
+def cardshuffle():
+    deck = newDeckOrder
+    return render_template("index.html", isNDO = isNDO(deck),displayDeck=formatDeck(deck))
+
 @app.route("/deckOrder", methods=["POST"])
 def deckOrder():
     shuffleType = request.form["shuffleType"]
-    
     shuffleCount = request.form.get("shuffleCount")
 
     if (shuffleCount.isdigit()):
@@ -84,7 +80,6 @@ def deckOrder():
                     output += formatDeck(deck) + "<br><br>"
             
             return render_template("deckOrder.html", shuffleCount=shuffleCount, shuffleType=shuffleType, output=output, notes=notes)
-    
     return redirect(url_for("cardshuffle")) #just return to original page if user provided rubbish
 
 if __name__ == "__main__":
