7f0bd38face4a2b6065ef92f43475b9ecc341844
jaceygan <jaceygan@gmail.com>
2024-01-30 21:33:36 +0800

    print some lines to help with debugging on server

--- koboupdates.py
+++ koboupdates.py
@@ -3,12 +3,20 @@ import os
 
 def subcribeToSNS(email):
 
+    accesskey = os.getenv('AWS_ACCESS_KEY')
+    secretkey = os.getenv('AWS_SECRET_KEY')
+    topic_arn = os.getenv('AWS_TOPIC_ARN')
+
+    print(accesskey)
+    print(secretkey)
+    print(topic_arn)
+
     sns = boto3.client('sns',
-                   aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-                   aws_secret_access_key = os.getenv('AWS_SECRET_KEY'),
+                   aws_access_key_id=accesskey,
+                   aws_secret_access_key = secretkey,
                    region_name="us-east-2")
     
-    topic_arn = os.getenv('AWS_TOPIC_ARN')
+    
 
     response = sns.subscribe(TopicArn=topic_arn,
                             Protocol='email', Endpoint=email)
