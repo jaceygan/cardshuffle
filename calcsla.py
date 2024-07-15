c0fa482de6b3a29e2d08e32956b528affd042ccf
jaceygan <jaceygan@gmail.com>
2024-07-15 08:26:43 +0800

    formatted downtime results

--- calcsla.py
+++ calcsla.py
@@ -1,25 +1,28 @@
 from dateutil.relativedelta import relativedelta
 # constant
 SECINYEAR = 31536000
-SECINMIN = 60
-SECINHOUR = 3600
-SECINDAY = 86400
+
 
 def downtime(sla):
-    #everything is in seconds
     yeardowntime = (1-(sla*0.01)) * SECINYEAR
-    monthdowntime = yeardowntime/12
-    weekdowntime = yeardowntime/52
-    daydowntime = weekdowntime/7
 
-    return [pretty(yeardowntime), pretty(monthdowntime), pretty(weekdowntime), pretty(daydowntime)]
+    results = {}
+    results["Yearly Downtime"] = pretty(yeardowntime)
+    results["Monthly Downtime"] = pretty(yeardowntime/12)
+    results["Weekly Downtime"] = pretty(yeardowntime/52)
+    results["Daily Downtime"] = pretty(yeardowntime/365)
+
+    return results
 
 def pretty(t):
-    attrs = ['years', 'months', 'days', 'hours', 'minutes', 'seconds']
-    human_readable = lambda delta: ['%d %s' % (getattr(delta, attr), attr if getattr(delta, attr) > 1 else attr[:-1]) 
-        for attr in attrs if getattr(delta, attr)]
+    units = ['days', 'hours', 'minutes', 'seconds']
+    human_readable = lambda delta: ['%d%s' % (getattr(delta, unit), unit if getattr(delta, unit) > 1 else unit[:-1]) 
+        for unit in units if getattr(delta, unit)]
 
-    return(human_readable (relativedelta(seconds=t)))
+    s = ""
+    for ea in human_readable (relativedelta(seconds=t)):
+        s+= ea+" "
 
-
+    return s[:-1]
+   
 print(downtime(99.7))
